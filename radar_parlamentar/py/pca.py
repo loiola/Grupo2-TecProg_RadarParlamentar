343a077fb11a1140418a93833c2a3c9160894f25
Yuri Loiola <loiola.com.br@hotmail.com>
2014-08-28 22:28:48 -0300

    Comentando arquivo pca.py

--- radar_parlamentar/py/pca.py
+++ radar_parlamentar/py/pca.py
@@ -56,8 +56,9 @@ Source: http://stackoverflow.com/questions/1730600/principal-component-analysis-
 from __future__ import division
 import numpy as np
 dot = np.dot
-    # import bz.numpyutil as nu
-    # dot = nu.pdot
+    
+    # Import bz.numpyutil as nu
+    # Dot = nu.pdot
 
 __version__ = "2010-04-14 apr"
 __author_email__ = "denis-bz-py at t-online dot de"
@@ -82,36 +83,49 @@ class PCA:
         return self.U[:, :n] * self.d[:n]
 
     # These 1-line methods may not be worth the bother;
-    # then use U d Vt directly --
+    # Then use U d Vt directly --
 
     def vars_pc( self, x ):
         n = self.npc
-        return self.d[:n] * dot( self.Vt[:n], x.T ).T  # 20 vars -> 2 principal
+
+        # 20 vars -> 2 principal
+        return self.d[:n] * dot( self.Vt[:n], x.T ).T  
 
     def pc_vars( self, p ):
         n = self.npc
+
+        # 2 PC -> 20 vars
         return dot( self.Vt[:n].T, (self.dinv[:n] * p).T ) .T  # 2 PC -> 20 vars
 
     def pc_obs( self, p ):
         n = self.npc
-        return dot( self.U[:, :n], p.T )  # 2 principal -> 1000 obs
+
+        # 2 principal -> 1000 obs
+        return dot( self.U[:, :n], p.T )  
 
     def obs_pc( self, obs ):
         n = self.npc
-        return dot( self.U[:, :n].T, obs ) .T  # 1000 obs -> 2 principal
+
+        # 1000 obs -> 2 principal
+        return dot( self.U[:, :n].T, obs ) .T  
 
     def obs( self, x ):
-        return self.pc_obs( self.vars_pc(x) )  # 20 vars -> 2 principal -> 1000 obs
+
+        # 20 vars -> 2 principal -> 1000 obs
+        return self.pc_obs( self.vars_pc(x) )  
 
     def vars( self, obs ):
-        return self.pc_vars( self.obs_pc(obs) )  # 1000 obs -> 2 principal -> 20 vars
+
+        # 1000 obs -> 2 principal -> 20 vars
+        return self.pc_vars( self.obs_pc(obs) )  
 
 
 class Center:
     """ A -= A.mean() /= A.std(), inplace -- use A.copy() if need be
         uncenter(x) == original A . x
     """
-        # mttiw
+        # Mttiw:
+        
     def __init__( self, A, axis=0, scale=True, verbose=1 ):
         self.mean = A.mean(axis=axis)
         if verbose:
