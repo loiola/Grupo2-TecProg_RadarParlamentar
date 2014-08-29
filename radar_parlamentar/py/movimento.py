68e59400c4e9635fffd389d3545741fe73f5f85f
Yuri Loiola <loiola.com.br@hotmail.com>
2014-08-28 22:18:37 -0300

    Comentando o arquivo movimento.py

--- radar_parlamentar/py/movimento.py
+++ radar_parlamentar/py/movimento.py
@@ -16,18 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: Transformar este script em uma função
-#       Investigar se é possível escolher a cor das bolhas.
 
 import numpy
 import analise
 import sys
+ 
+# Modifiable parameters:
+# Political parties to be included in the analyzes:
 
-############################# 
-## PARAMETROS MODIFICÁVEIS ##
-############################# 
-
-# Partidos a serem incluídos nas análises:
 parts = [u'PMDB', u'PTB', u'PDT', u'PT', u'DEM', u'PCdoB', u'PSB', u'PSDB', u'PSC', u'PMN', u'PPS', u'PV', u'PTdoB', u'PP', u'PHS', u'PRB', u'PSOL', u'PR', u'PSD']
 
 ano_inicial = 2002
@@ -36,15 +32,15 @@ ano_final = 2011
 arquivo_de_saida = 'colar_num_html.txt'
 
 
-############################# 
-##  Programa em si:        ##
-############################# 
+ 
+# Algorithm:        
 
 anuais = [] # lista de objetos do tipo Analise (que serão análises anuais)
 
 anos = range(ano_inicial,ano_final+1)
 
-# Fazer as PCAs:
+# Make PCAs:
+
 for ano in anos:
     anuais.append(analise.Analise(str(ano)+'-01-01', str(ano)+'-12-31', [], parts))
 
@@ -57,8 +53,8 @@ for a in anuais:
     sys.stdout.flush()
 
 
+# Auxiliary functions:
 
-# Funções auxiliares:
 def quantidade_movimento(i,graus=0,espelho=0):
     """Calculates the amount of movement between the instant i (corresponding to the year years [i]) and the time i + 1.
      When calculating the time i has the rotated axes (degree value between 0 and 360) and the first axis multiplied by -1 if the mirror = 0.
@@ -86,14 +82,18 @@ def matrot(graus):
 
 print ' '
 print 'Espelhando e rotacionando...'
-# Rodar e espelhar eixos conforme a necessidade:
-# O sentido dos eixos que resultam na PCA é arbitrário, e se não dermos tanta importância ao significado do eixo x e do eixo y, mas sim principalmente à distância entre os partidos dois a dois que se reflete no plano, a rotação dos eixos é também arbitrária. Ao relacionar análises feitas em períodos de tempo diferentes (no caso, anos), os eixos de uma análise não têm relação com os da análise seguinte (pois foram baseados em votações distintas), então se fixarmos os eixos do ano i mais recente, o ano i-1 pode ter o eixo x espelhado ou não, e pode sofrer uma rotação de ângulo qualquer.
-# Gostaríamos que estas transformações fossem tais que minimizasse o movimento dos partidos: por exemplo se no ano i o partido A resultou no lado esquerdo do gráfico, e o partido B no lado direito, mas no ano i-1 o posicionamento resultou inverso, seria desejável espelhar o eixo x, ou então rodar os eixos de 180 graus.
-# Isso é alcançado através do algoritmo abaixo, de minimização da 'quantidade de movimento' total com a variação da rotação dos eixos e espelhamento do eixo x. Entre dois anos, esta quantidade de movimento é definida pela soma das distâncias euclidianas caminhadas pelos partidos ponderadas pelo tamanho do partido [no ano mais recente].
-for i in range(len(anos)-2,-1,-1): # indices dos anos, de tras pra frente
+
+
+# Indices of years, backwards:
+
+for i in range(len(anos)-2,-1,-1): 
     print anos[i]
-    qm_min = 1000000 # quero minimizar as quantidades de movimento
-    campeao = (0,0) # (espelhar, graus)
+
+    # Minimizing the amount of motion:
+    qm_min = 1000000 
+
+    # Mirror, degrees:
+    campeao = (0,0) 
     for espelhar in [0,1]:
         for graus in [0,45,90,135,180,225,270,315]:
             qm_agora = quantidade_movimento(i,graus,espelhar)
@@ -102,14 +102,19 @@ for i in range(len(anos)-2,-1,-1): # indices dos anos, de tras pra frente
                 campeao = (espelhar, graus)
                 qm_min = qm_agora
     print campeao
-    if campeao[0] == 1: # espelhar
+    if campeao[0] == 1: 
+
+        # Mirror:
         dados[i] = numpy.dot( dados[i], numpy.array([[-1.,0.],[0.,1.]]) )
-    if campeao[1] != 0: # rotacionar
+    if campeao[1] != 0: 
+
+        # Rotate:
         dados[i] = numpy.dot( dados[i], matrot(campeao[1]) )
 
 print 'Fim'
 
-# Escrever arquivo:
+# Writing file:
+
 f = open(arquivo_de_saida,'w')
 f.write("""  <script type="text/javascript" src="http://www.google.com/jsapi"></script>
   <script type="text/javascript">
@@ -125,12 +130,12 @@ f.write("data.addColumn('number', 'Eixo1');\n")
 f.write("data.addColumn('number', 'Eixo2');\n")
 f.write("data.addColumn('number', 'Tamanho');\n")
 f.write("data.addRows([\n")
-for ia in range(len(anuais)): # datas
+for ia in range(len(anuais)): 
     a = anuais[ia]
     d_ano = int(a.data_final[0:4])
-    d_mes = int(a.data_final[5:7])-1 # em js meses sao de 0 a 11
+    d_mes = int(a.data_final[5:7])-1 
     d_dia = int(a.data_final[8:10])
-    for ip in range(len(parts)): # partidos
+    for ip in range(len(parts)): 
         linha = "  ['%s',new Date(%d,%d,%d), %f,%f,%d],\n" % (parts[ip],d_ano,d_mes,d_dia,dados[ia][ip,0],dados[ia][ip,1],a.tamanho_partido[ip])
         f.write(linha)
 f.seek(-2,1)
