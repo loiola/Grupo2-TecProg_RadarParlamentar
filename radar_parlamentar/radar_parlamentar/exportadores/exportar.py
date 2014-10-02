d6b634f36e1668e1cdce128c6eb6d73bf31b5797
Yeltsin Suares <yeltsin.lobato@hotmail.com>
2014-10-02 10:55:06 -0300

    Indenting exportar.py

--- radar_parlamentar/radar_parlamentar/exportadores/exportar.py
+++ radar_parlamentar/radar_parlamentar/exportadores/exportar.py
@@ -28,7 +28,6 @@ import os
 
 MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 def main():
     serialize_partido()
     serialize_casa_legislativa()
@@ -38,7 +37,6 @@ def main():
     serialize_votacao()
     serialize_voto()
 
-
 def serialize_partido():
     XMLSerializer = serializers.get_serializer("xml")
     xml_serializer = XMLSerializer()
@@ -46,13 +44,11 @@ def serialize_partido():
     out = open(filepath, "w")
     xml_serializer.serialize(models.Partido.objects.all(), stream=out)
 
-
 def serialize_casa_legislativa():
     XMLSerializer = serializers.get_serializer("xml")
     xml_serializer = XMLSerializer()
     out = open(os.path.join(MODULE_DIR, 'dados/casa_legislativa.xml'), "w")
     xml_serializer.serialize(models.CasaLegislativa.objects.all(), stream=out)
-
 
 def serialize_parlamentar():
     XMLSerializer = serializers.get_serializer("xml")
@@ -60,13 +56,11 @@ def serialize_parlamentar():
     out = open(os.path.join(MODULE_DIR, 'dados/parlamentar.xml'), "w")
     xml_serializer.serialize(models.Parlamentar.objects.all(), stream=out)
 
-
 def serialize_legislatura():
     XMLSerializer = serializers.get_serializer("xml")
     xml_serializer = XMLSerializer()
     out = open(os.path.join(MODULE_DIR, 'dados/legislatura.xml'), "w")
     xml_serializer.serialize(models.Legislatura.objects.all(), stream=out)
-
 
 def serialize_proposicao():
     XMLSerializer = serializers.get_serializer("xml")
@@ -74,19 +68,18 @@ def serialize_proposicao():
     out = open(os.path.join(MODULE_DIR, 'dados/proposicao.xml'), "w")
     xml_serializer.serialize(models.Proposicao.objects.all(), stream=out)
 
-
 def serialize_votacao():
     XMLSerializer = serializers.get_serializer("xml")
     xml_serializer = XMLSerializer()
     out = open(os.path.join(MODULE_DIR, 'dados/votacao.xml'), "w")
     xml_serializer.serialize(models.Votacao.objects.all(), stream=out)
 
-
 def serialize_voto():
     XMLSerializer = serializers.get_serializer("xml")
     xml_serializer = XMLSerializer()
     out = open(os.path.join(MODULE_DIR, 'dados/voto.xml'), "w")
     voto = models.Voto.objects.all()
+
     for e in voto:
         e.id = None
     xml_serializer.serialize(voto, stream=out)
