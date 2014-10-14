df567a7ca202dfe958636fe0792810c97551a463
Yeltsin Suares <yeltsin.lobato@hotmail.com>
2014-10-14 18:32:41 -0300

    Simplifying code, rename variables and adjusting

--- radar_parlamentar/py/scripts/analise_tipos.py
+++ radar_parlamentar/py/scripts/analise_tipos.py
@@ -22,28 +22,30 @@
 import proposicoes
 
 proposicoes = proposicoes.parse_html()
-pl = plp = pdc = mpv = pec = 0
+
+initial_quantity = 0
+ordinary_law_projects = complementary_law_projects = legislative_decree_projects = provisional_measures = amendment_constitution = initial_quantity
 
 # Performs proposition count for each type
 for propositions in proposicoes:
   if (propositions['tipo'] == 'PL'):
-    pl += 1
+    ordinary_law_projects += 1
   elif (propositions['tipo'] == 'PLP'):
-    plp += 1
+    complementary_law_projects += 1
   elif (propositions['tipo'] == 'PDC'):
-    pdc += 1
+    legislative_decree_projects += 1
   elif (propositions['tipo'] == 'MPV'):
-    mpv += 1
+    provisional_measures += 1
   elif (propositions['tipo'] == 'PEC'):
-    pec += 1
+    amendment_constitution += 1
 
 # Performs total count of propositions and shows the result for each type
 total = len(proposicoes)
 print('Votações na câmara em 2011')
 print('%d proposições' % total)
-print('%d PLs (%d%s)' % (pl, pl/total*100, '%'))
-print('%d PLPs (%d%s)' % (plp, plp/total*100, '%'))
-print('%d PDCs (%d%s)' % (pdc, pdc/total*100, '%'))
-print('%d MPVs (%d%s)' % (mpv, mpv/total*100, '%'))
-print('%d PECs (%d%s)' % (pec, pec/total*100, '%'))
+print('%d PLs (%d%s)' % (ordinary_law_projects, ordinary_law_projects/total*100, '%'))
+print('%d PLPs (%d%s)' % (complementary_law_projects, complementary_law_projects/total*100, '%'))
+print('%d PDCs (%d%s)' % (legislative_decree_projects, legislative_decree_projects/total*100, '%'))
+print('%d MPVs (%d%s)' % (provisional_measures, provisional_measures/total*100, '%'))
+print('%d PECs (%d%s)' % (amendment_constitution, amendment_constitution/total*100, '%'))
 #print('Checksum: %d' % (pl+plp+pdc+mpv+pec))
