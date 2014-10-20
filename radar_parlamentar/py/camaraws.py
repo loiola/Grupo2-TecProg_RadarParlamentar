a2589fc17d0910666c8b565daac3acbc760697db
Yuri Loiola <loiola.com.br@hotmail.com>
2014-10-20 00:38:43 -0200

    Renomeando variáveis e métodos do arquivo camaraws.py

--- radar_parlamentar/py/camaraws.py
+++ radar_parlamentar/py/camaraws.py
@@ -15,32 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Module camaraws -- requirements for the chamber Web Services
+"""Module CamaraWS -- requirements for the chamber Web Services
 
 Functions:
-obter_votacao -- gets votings and details of proposition, giving the ID.
-obter_nomeProp_porid -- gets name of proposition, giving the ID.."""
+get_votings: gets votings and details of proposition, giving the ID.
+get_proposition_name_by_id: gets name of proposition, giving the ID."""
 
 from model import Proposicao
 import urllib2
 import xml.etree.ElementTree as etree
 import io
 
-OBTER_VOTACOES_PROPOSICAO = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterVotacaoProposicao?tipo=%s&numero=%s&ano=%s'
-OBTER_INFOS_PROPOSICAO = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterProposicao?tipo=%s&numero=%s&ano=%s'
-OBTER_INFOS_PORID = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterProposicaoPorID?idProp=%s'
+GET_VOTINGS_PROPOSITION = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterVotacaoProposicao?tipo=%s&numero=%s&ano=%s'
+GET_INFO_PROPOSITION = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterProposicao?tipo=%s&numero=%s&ano=%s'
+GET_INFOS_BY_ID = 'http://www.camara.gov.br/sitcamaraws/Proposicoes.asmx/ObterProposicaoPorID?idProp=%s'
 
-def obter_votacao(tipo, num, ano):
+def get_votings(tipo, num, ano):
     """Get votings ande details of a propositions.
 
     Arguments:
-    tipo, num, ano -- strings that caracterize a proposition
+    tipo, num, ano: strings that caracterize a proposition
 
-    Retorns:
-    A propostions as an object class model.Proposicao.
+    Returns:
+    A propostion as an object class model.Proposicao.
     If the proposition doesn't be found or doesn't have votings, returns None."""
 
-    url  = OBTER_VOTACOES_PROPOSICAO % (tipo, num, ano)
+    url = GET_VOTINGS_PROPOSITION % (tipo, num, ano)
     try:
         request = urllib2.Request(url)
         xml = urllib2.urlopen(request).read()
@@ -48,49 +48,49 @@ def obter_votacao(tipo, num, ano):
         return None
 
     try:
-        prop = Proposicao.fromxml(xml)
+        proposition = Proposicao.fromxml(xml)
     except:
         return None
-    if not isinstance(prop, Proposicao): 
+    if not isinstance(proposition, Proposicao):
         return None
 
     # Here is the xml with more details about the proposition:
-    xml = obter_proposicao(tipo, num, ano) 
+    xml = get_proposition(tipo, num, ano)
 
     tree = etree.fromstring(xml)
-    prop.id = tree.find_legislature('idProposicao').text
-    prop.ementa = tree.find_legislature('Ementa').text
-    prop.explicacao = tree.find_legislature('ExplicacaoEmenta').text
-    prop.situacao = tree.find_legislature('Situacao').text
-    return prop
+    proposition.id = tree.find_legislature('idProposicao').text
+    proposition.ementa = tree.find_legislature('Ementa').text
+    proposition.explicacao = tree.find_legislature('ExplicacaoEmenta').text
+    proposition.situacao = tree.find_legislature('Situacao').text
+    return proposition
 
-def obter_proposicao(tipo, num, ano):
+def get_proposition(tipo, num, ano):
     """Get details of the propositions by type, number and year
 
     Arguments:
-    tipo, num, ano -- strings that caracterize the propositions.
+    tipo, num, ano: strings that caracterize the propositions.
 
     Return:
-    A xml representing a proposition as an object of bytes classe."""
+    A xml representing a proposition as an object of bytes."""
 
-    url = OBTER_INFOS_PROPOSICAO % (tipo, num, ano)
+    url = GET_INFO_PROPOSITION % (tipo, num, ano)
     request = urllib2.Request(url)
     xml = urllib2.urlopen(request).read()
     return xml
 
 
-def obter_nomeProp_porid(idProp):
+def get_proposition_name_by_id(idProp):
     """Giving the id, gets the name of proposition.
     For exemple: obter_nomeProp_porid(513512) retorns the string "MPV 540/2011"
 
     Arguments:
-    idprop -- integer used as unique identificator of a proposition in webservice.
+    idprop: integer used as unique identificator of a proposition in webservice.
 
     Returns:
     A string with type, number and year of proposition, for exemple "MPV 540/2011".
     If the proposition doesn't be found, returns None."""
 
-    url = OBTER_INFOS_PORID % (idProp)
+    url = GET_INFOS_BY_ID % (idProp)
     try:
         request = urllib2.Request(url)
         xml = urllib2.urlopen(request).read()
@@ -98,8 +98,8 @@ def obter_nomeProp_porid(idProp):
         return None
 
     try:
-        nomeProp = Proposicao.fromxmlid(xml)
+        propositionName = Proposicao.fromxmlid(xml)
     except:
         return None
 
-    return nomeProp
+    return propositionName
